63edb848be2fef1ef86a15199959dd50fd61644e
Vũ Văn Minh <min6th@gmail.com>
2023-04-18 08:45:28 +0700

    add logs

--- stake-gateway/migration/2023033001_trading_rooms.js
+++ stake-gateway/migration/2023033001_trading_rooms.js
@@ -2,14 +2,15 @@ const logger = require("../common/logger");
 const { TradingRoom } = require("../models/TradingRoom");
 const baseMigration = require("./BaseMigration");
 
-const trading_room_2023033001 = baseMigration.create('2023033001_trading_rooms', () => {
+const trading_room_2023033001 = baseMigration.create('2023033001_trading_rooms', async () => {
     let roomSymbols = (process.env.TRADING_SYMBOLS || 'BTCUSDT ETHUSDT BNBUSDT').split(' ');
     logger.debug('migration', `${roomSymbols}`);
     for (let roomSymbol of roomSymbols) {
         let tradingRoom = new TradingRoom({
             symbol: roomSymbol,
         });
-        tradingRoom.save();
+        tradingRoom = await tradingRoom.save();
+        logger.debug('migrations', `${JSON.stringify(tradingRoom)}`);
     }
 });
 module.exports = trading_room_2023033001;
