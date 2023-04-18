196b948b162991342154395dcb579ec48c4658fe
Vũ Văn Minh <min6th@gmail.com>
2023-04-18 08:53:21 +0700

    fix wrong path

--- stake-gateway/migration/2023033001_trading_rooms.js
+++ stake-gateway/migration/2023033001_trading_rooms.js
@@ -1,11 +1,11 @@
 const logger = require("../common/logger");
-const { TradingRoom } = require("../models/TradingRoom");
+const TradingRoom = require("../models/TradingRoom");
 const baseMigration = require("./BaseMigration");
 
 const trading_room_2023033001 = baseMigration.create('2023033001_trading_rooms', async () => {
     let roomSymbols = (process.env.TRADING_SYMBOLS || 'BTCUSDT ETHUSDT BNBUSDT').split(' ');
-    logger.debug('migration', `${roomSymbols}`);
     for (let roomSymbol of roomSymbols) {
+        logger.debug('migrations', `${roomSymbol}`);
         let tradingRoom = new TradingRoom({
             symbol: roomSymbol,
         });
